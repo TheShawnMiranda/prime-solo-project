d18f667e434e429152701333efd1b3fa18b39aec
Shawn Miranda <theshawnmiranda@gmail.com>
2020-03-29 14:56:28 -0500

    put route added to edit data on file

--- src/redux/sagas/editDataSaga.js
+++ src/redux/sagas/editDataSaga.js
@@ -4,9 +4,7 @@ import { put, takeLatest } from 'redux-saga/effects';
 // worker Saga: will be fired on "FETCH_USER" actions
 function* postNewData(action) {
     try {
-
-        const response = yield axios.put('/data', action.payload);
-
+        yield axios.put('/data', action.payload);
     } catch (error) {
         console.log('User get request failed', error);
     }
