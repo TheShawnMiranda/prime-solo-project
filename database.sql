837b83731328a2a52773db986da35a942f91c98f
Shawn Miranda <theshawnmiranda@gmail.com>
2020-11-11 12:41:56 -0600

    Added lines from postico

--- database.sql
+++ database.sql
@@ -23,7 +23,16 @@ CREATE TABLE "data" (
 );
 
 CREATE TABLE "matches" (
-	"id" SERIAL PRIMARY KEY,
+	"id" SERIAL PRIMARY KEY REFERENCES "data",
 	"donor_id" INT REFERENCES "data",
 	"recipient_id" INT REFERENCES "data"
 );
+
+DROP TABLE "matches";
+
+SELECT recipient_name, recipient_blood_type, recipient_height, recipient_weight, recipient_age, recipient_organ FROM data
+JOIN matches ON matches.recipient_id = 1;
+
+SELECT donor_name, donor_blood_type, donor_height, donor_weight, donor_age, donor_organ FROM data
+JOIN matches ON matches.donor_id = data.id
+WHERE matches.id = 1 AND 
